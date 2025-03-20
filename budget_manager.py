aebd47ecdff784cab447cc05b5b29871285acbe4
yonio <doesitmatter@shiz.shiz>
2025-03-20 18:17:11 +0200

    Added code for user input

--- budget_manager.py
+++ budget_manager.py
@@ -1,12 +1,53 @@
 budget_data = { "balance": 0,
 "transactions": {
-"income": [],
-"expense": []
+"income": [], ### (300, "Drugs"), (400, "Sex")
+"expense": [] ### (500, "Rock N' Roll)
 }
 }
 
-def read_from_menu(menu:list):
-    pass
+def get_valid_option(options, prompt):
+    while True:
+        try:
+            if prompt:
+                print(prompt)
+
+            for idx, option in enumerate(options, start=1):
+                print(f"{idx}. {option}")
+
+            # Get user input
+            selection=int(input(f"Please select an option (1-{len(options)}): "))
+
+            if 1 <= selection <= len(options):
+                return selection
+            else:
+                print("Invalid selection, please choose a valid number from the list.\n")
+        except ValueError:
+            print("Invalid input, please enter a valid number.\n")
+
+
+def get_valid_float(prompt):
+    while True:
+        try:
+            if prompt:
+                prompt=prompt.strip()+": "
+            value = float(input(prompt))
+
+            if value > 0:
+                return value
+            else:
+                print("Please enter a value greater than 0.\n")
+        except ValueError:
+            print("Invalid input, please enter a valid number.\n")
+
+
+def get_valid_string(prompt):
+    while True:
+        value = input(prompt).strip()
+
+        if value:
+            return value
+        else:
+            print("Invalid input, the string cannot be empty.\n")
 
 # Budget Manager
 # 1. Add Income
@@ -17,9 +58,7 @@ def read_from_menu(menu:list):
 
 print("Welcome to the Budget Manager\n\n")
 
-# print menu
-
-user_input=read_from_menu(["Add Income",
+user_input=get_valid_option(["Add Income",
                            "Add Expense",
                            "Show Balance",
                            "Show Transaction History",
@@ -28,21 +67,15 @@ user_input=read_from_menu(["Add Income",
 while user_input != 5:
     match (user_input):
         case 1:
-            pass
-            ### Add income
+            sum=get_valid_float("Please insert the sum to add")
+            comment=get_valid_string("Please insert a comment for the income")
             #
-            # read amount
-            # read comment
-            #
-            ### add_income(budget_data, sum, comment)
+            ### add_income(budget_data, sum, comment) ### Don't forget to calculate balance
         case 2:
-            pass
-            ### Add expense
+            sum = get_valid_float("Please insert the sum to reduce")
+            comment = get_valid_string("Please insert a comment for the expense")
             #
-            # read amount
-            # read comment
-            #
-            ### add_expense(budget_data, sum, comment)
+            ### add_expense(budget_data, sum, comment) ### Don't forget to calculate balance
         case 3:
             pass
             ### show balance
@@ -56,7 +89,7 @@ while user_input != 5:
         case _:
             print("How did you get here????")
 
-    user_input = read_from_menu(["Add Income",
+    user_input = get_valid_option(["Add Income",
                                  "Add Expense",
                                  "Show Balance",
                                  "Show Transaction History",
